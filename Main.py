ee751a1252b3a8903bc6dde8452beecbceff4eef
DublinJim <dublinjim.jk@gmail.com>
2025-06-11 08:54:17 -0400

    Added a better age method

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
-from MyClasses import Pet
+from MyClasses import Dog, Pet, Ultra
 
 myPet = Pet("Fido", 5)
 
 print(myPet.__dict__)
+print("This is myPet Object ID: " + str(id(myPet)))
 myPet.howOld()
 myPet.yearBorn()
+myDog = Dog("Fido", 5, "Golden Retriever")
+myDog.saybreed()
+
+myUltra = Ultra(myPet, myDog)
+
+myUltra.Pet.howOld()
+myUltra.Dog.saybreed()
--- MyClasses.py
+++ MyClasses.py
@@ -10,7 +10,7 @@ class Pet:
         self.current_year = date.today().year
 
     def howOld(self):
-        print(self.age)
+        print("aged", self.age)
 
     def yearBorn(self):
         print("Born in", self.current_year - self.age)
@@ -20,3 +20,12 @@ class Dog(Pet):
     def __init__(self, name, age, breed):
         super().__init__(name, age)
         self.breed = breed
+
+    def saybreed(self):
+        print(self.breed)
+
+
+class Ultra:
+    def __init__(self, Pet, Dog):
+        self.Pet = Pet
+        self.Dog = Dog
